305edf4906731b9cbba1dc06833290bb7c2840f7
Stanislav <gerosenkostas@gmail.com>
2026-09-07 15:23:27 +0500

    fix: clean up procedural world readability

--- scripts/gameplay13-layout.js
+++ scripts/gameplay13-layout.js
@@ -6,10 +6,11 @@ for(const image of images){const p='assets/game/'+image;if(!resources.some(r=>r.
 const clone=v=>structuredClone(v),obj=n=>layout.objects.find(o=>o.name===n),all=n=>layout.instances.filter(i=>i.name===n);
 const type=(name,source,image)=>{let o=obj(name);if(!o){o=clone(obj(source));o.name=name;layout.objects.push(o);}if(image)o.animations[0].directions[0].sprites[0].image=image;};
 const add=(name,source,p)=>{const i=clone(all(source)[0]);i.name=name;i.persistentUuid=crypto.randomUUID();Object.assign(i,p);layout.instances.push(i);};
-for(const n of ['ProcLoot','ProcScrap','ProcEnergy','ProcData','ProcHeavy','ProcDecor','ProcHazard','ProcPOI','RegionReveal','G13Signal','G13Key','G13Gate','G13Cache','Laser13','Emitter13','CorridorWall','ShipModules','MeteorTelegraph','Dev13Panel','Dev13Text'])layout.instances=layout.instances.filter(i=>i.name!==n);
+for(const n of ['ProcLoot','ProcScrap','ProcEnergy','ProcData','ProcHeavy','ProcDecor','ProcHazard','ProcPOI','RegionReveal','G13Signal','G13Key','G13Gate','G13Cache','Laser13','Emitter13','CorridorWall','ShipModules','MeteorTelegraph','Dev13Panel','Dev13Text','ModalDim13'])layout.instances=layout.instances.filter(i=>i.name!==n);
 type('ProcLoot','RareContainer','assets/game/salvage_energy.svg');type('ProcScrap','CommonSalvage');type('ProcEnergy','RareContainer','assets/game/salvage_energy.svg');type('ProcData','RareContainer','assets/game/salvage_data.svg');type('ProcHeavy','RareContainer','assets/game/salvage_heavy.svg');type('ProcDecor','AmbientDebris');type('ProcHazard','FastDebris');type('ProcPOI','SatelliteWreck','assets/game/poi_relay.svg');
 type('RegionReveal','FieldLabel');type('G13Signal','SecretPortalGlow','assets/game/signal_gate.svg');type('G13Key','AccessKey','assets/game/access_key.svg');type('G13Gate','SecretPortalGlow','assets/game/signal_gate.svg');type('G13Cache','SecretCache','assets/game/secret_cache.svg');
 type('Laser13','LaserBeam','assets/game/laser_beam.svg');type('Emitter13','LaserEmitter','assets/game/laser_emitter.svg');type('CorridorWall','SatelliteWreck');type('ShipModules','ScrapGlow','assets/game/ship_modules.svg');type('MeteorTelegraph','FieldLabel');type('Dev13Panel','HudPanel');type('Dev13Text','FieldLabel');
+type('ModalDim13','DamageFlash');
 for(let i=0;i<36;i++)add('ProcScrap','CommonSalvage',{x:-2000,y:-2000,width:48,height:48,zOrder:7,layer:'World'});
 for(let i=0;i<12;i++)add('ProcEnergy','RareContainer',{x:-2000,y:-2000,width:54,height:54,zOrder:7,layer:'World'});
 for(let i=0;i<8;i++)add('ProcData','RareContainer',{x:-2000,y:-2000,width:58,height:58,zOrder:8,layer:'World'});
@@ -23,7 +24,9 @@ for(let i=0;i<9;i++){add('Laser13','LaserBeam',{x:-2000,y:-2000,width:300,height
 for(let i=0;i<18;i++)add('CorridorWall','SatelliteWreck',{x:-2000,y:-2000,width:160,height:90,zOrder:4,layer:'World'});
 add('ShipModules','ScrapGlow',{x:-2000,y:-2000,width:92,height:92,zOrder:6,layer:'World'});add('MeteorTelegraph','FieldLabel',{x:-2000,y:-2000,width:440,height:48,zOrder:10,layer:'World'});
 add('Dev13Panel','HudPanel',{x:24,y:690,width:570,height:350,zOrder:80,layer:'HUD'});add('Dev13Text','FieldLabel',{x:48,y:715,width:520,height:300,zOrder:81,layer:'HUD'});
+add('ModalDim13','DamageFlash',{x:0,y:0,width:1920,height:1080,zOrder:8,layer:'HUD'});
 const reveal=obj('RegionReveal');if(reveal){reveal.characterSize=32;reveal.content.characterSize=32;reveal.content.textAlignment='center';}
+for(const item of layout.instances.filter(i=>i.name==='SecretLabel'))Object.assign(item,{x:720,y:920,width:480,height:52,layer:'HUD',zOrder:31});
 const dev=obj('Dev13Text');if(dev){dev.characterSize=16;dev.content.characterSize=16;dev.content.textAlignment='left';}
 layout.r=0;layout.v=0;layout.b=0;
 fs.writeFileSync(file,JSON.stringify(project,null,2)+'\n');console.log('Gameplay Overhaul 13 layout applied');
