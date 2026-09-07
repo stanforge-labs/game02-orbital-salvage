c79d927155defe16b11888f97b85c028287683b2
Stanislav <gerosenkostas@gmail.com>
2026-09-07 11:48:57 +0500

    feat: add seeded gameplay overhaul and secret route

--- scripts/apply-integrated-pass10.js
+++ scripts/apply-integrated-pass10.js
@@ -6,4 +6,5 @@ run('scripts/smart08-layout.js');
 run('scripts/integrated-pass10-layout.js');
 run('scripts/release11-layout.js');
 run('scripts/release12-layout.js');
+run('scripts/gameplay13-layout.js');
 run('scripts/audit-project.js');
--- scripts/update-runtime-feature.js
+++ scripts/update-runtime-feature.js
@@ -97,5 +97,6 @@ require('./smart08-runtime')(layout);
 require('./integrated-pass10-runtime')(layout);
 require('./release11-runtime')(layout);
 require('./release12-runtime')(layout);
+require('./gameplay13-runtime')(layout);
 fs.writeFileSync(file, JSON.stringify(project, null, 2) + '\n', 'utf8');
 console.log('Gameplay Depth 07 runtime applied');
